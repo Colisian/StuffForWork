4f475098a635eb9cad2f1597512f35d3ee468fb6
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-03-06 10:52:54 -0500

    Added Comments.

--- Panopto/DeleteFiles/ScheduleClean.ps1
+++ Panopto/DeleteFiles/ScheduleClean.ps1
@@ -1,9 +1,16 @@
 
 
 $Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-ExecutionPolicy Bypass -File 'C:\PerfLogs\CleanPanoptoRecorder.ps1'"
+
+# Set the trigger for the scheduled task to weekly on Sundays at 3 AM
 $Trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Sunday -At 3am
+
+# Configure settings for the scheduled task (e.g., allow the task to run on batteries)
 $Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
+
+# define the user account under which the task runs, using SYSTEM for highest privileges
 $Principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 
+#Create task
 Register-ScheduledTask -TaskName "ClearPanoptoRecorderFiles" -Action $Action -Trigger $Trigger -Settings $Settings -Principal $Principal
 
