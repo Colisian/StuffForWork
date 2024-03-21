76b11bf57c1c89e447855cad0ec6b7d2a29a1469
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-03-21 10:54:04 -0400

    Added logging file creation.

--- Panopto/DeleteFiles/install.cmd
+++ Panopto/DeleteFiles/install.cmd
@@ -1,4 +1,7 @@
 @echo off
+REM Check if the LastRun.txt file exists, create it if it doesn't
+if not exist "C:\LastRun.txt" echo Created on %DATE% %TIME% > "C:\LastRun.txt"
+
 REM Run the PowerShell script
 PowerShell.exe -ExecutionPolicy Bypass -File ".\ScheduleAndDeletePanoptoRec.ps1"
 
