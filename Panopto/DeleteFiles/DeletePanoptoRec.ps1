f6a4fd1460c5a15855516c562924c8196fbf01a4
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-03-21 09:05:28 -0400

    Combined delete and task scheduler script.

---
+++ Panopto/DeleteFiles/DeletePanoptoRec.ps1
@@ -0,0 +1,16 @@
+
+
+$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Execute "PowerShell.exe" -Argument "-ExecutionPolicy Bypass -Command `"$path = 'C:\PanoptoRecorder'; $days = 7; Get-ChildItem -Path `$path -Recurse | Where-Object { `$_.LastWriteTime -lt (Get-Date).AddDays(-`$days) -and `$_.FullName -notmatch 'C:\\\PanoptoRecorder\\\eventLogs' -and `$_.FullName -notmatch 'C:\\\PanoptoRecorder\\\UCSUploads' } | Remove-Item -Force -Recurse -Confirm:`$false`""
+
+# Set the trigger for the scheduled task to weekly on Sundays at 3 AM
+$Trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Sunday -At 3am
+
+# Configure settings for the scheduled task (e.g., allow the task to run on batteries)
+$Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
+
+# define the user account under which the task runs, using SYSTEM for highest privileges
+$Principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+
+#Create task
+Register-ScheduledTask -TaskName "ClearPanoptoRecorderFiles" -Action $Action -Trigger $Trigger -Settings $Settings -Principal $Principal
+
