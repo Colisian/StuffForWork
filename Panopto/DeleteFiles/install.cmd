ee4450577f399f9df037d51501bb75c72819acce
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-03-21 10:11:56 -0400

    Adjusted install.cmd

--- Panopto/DeleteFiles/install.cmd
+++ Panopto/DeleteFiles/install.cmd
@@ -1,3 +1,15 @@
 @echo off
-copy ".\CleanPanoptoRecorder.ps1" "C:\PerfLogs\CleanPanoptoRecorder.ps1" 
-PowerShell.exe -ExecutionPolicy Bypass -File ".\ScheduleClean.ps1" 
+REM Run the PowerShell script
+PowerShell.exe -ExecutionPolicy Bypass -File ".\ScheduleAndDeletePanoptoRec.ps1"
+
+REM Check if the PowerShell script ran successfully
+if %ERRORLEVEL% -eq 0 (
+    echo Script ran successfully
+    REM Create or update the file used for detection rules
+    echo %DATE% %TIME% > "C:\PerfLogs\LastRun.txt"
+) else (
+    echo Script failed to run
+)
+
+REM Exit
+exit /b 0
