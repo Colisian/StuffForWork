5ba99f901546cfd38b87ca27893b33f3ffc3ae26
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-03-21 09:24:57 -0400

    Adjusted $Action script.

--- Panopto/DeleteFiles/DeletePanoptoRec.ps1
+++ Panopto/DeleteFiles/DeletePanoptoRec.ps1
@@ -1,6 +1,5 @@
 
-
-$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-ExecutionPolicy Bypass -Command `"$path = 'C:\PanoptoRecorder'; $days = 2; Get-ChildItem -Path `$path -Recurse | Where-Object { `$_.LastWriteTime -lt (Get-Date).AddDays(-`$days) -and `$_.FullName -notmatch 'C:\\\PanoptoRecorder\\\eventLogs' -and `$_.FullName -notmatch 'C:\\\PanoptoRecorder\\\UCSUploads' } | Remove-Item -Force -Recurse -Confirm:`$false`""
+$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-ExecutionPolicy Bypass -Command `"$path = 'C:\PanoptoRecorder'; $days = 2; Get-ChildItem -Path `$path -Recurse | Where-Object { `$_.LastWriteTime -lt (Get-Date).AddDays(-`$days) -and `$_.FullName -notmatch 'C:\\PanoptoRecorder\\eventLogs' -and `$_.FullName -notmatch 'C:\\PanoptoRecorder\\UCSUploads' } | Remove-Item -Force -Recurse -Confirm:`$false`""
 
 # Set the trigger for the scheduled task to weekly on Sundays at 3 AM
 $Trigger = New-ScheduledTaskTrigger -Daily -At 2am
