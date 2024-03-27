0334d43254f5d6a8365ccdea9e1b811c3a2c0d31
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-03-27 12:56:10 -0400

    Added logic for delete user profiles script.

---
+++ General/DeleteUserProfile.ps1
@@ -0,0 +1,17 @@
+# Get all user profiles, exluding system profiles
+$userProfiles = Get-WmiObject -Class Win32_UserProfile | Where-Object { -not $_.Special  } -and 
+$_.LocalPath.split('\')[-1] -ne "CSPAdmin"
+
+# Iterate through each user profile
+foreach ($profile in $userProfiles) {
+    try {
+        #mae sure the profile is not the system account
+        if($profile -ne $null -and !$profile.Special){
+            Write-Host "Deleting user profile: $($profile.LocalPath.split('\')[-1])"
+            $profile.Delete()
+        }
+    }
+    catch {
+        Write-Host "Failed to delete user profile: $($profile.LocalPath.split('\')[-1])"
+    }
+}
