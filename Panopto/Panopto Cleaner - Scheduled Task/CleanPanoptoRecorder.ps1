d401ecdd7354e1755d59ba442ce565a17daa1e39
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-04-08 12:26:31 -0400

    Changed filtered days.

--- Panopto/Panopto Cleaner - Scheduled Task/CleanPanoptoRecorder.ps1
+++ Panopto/Panopto Cleaner - Scheduled Task/CleanPanoptoRecorder.ps1
@@ -4,8 +4,8 @@ $path = "C:\PanoptoRecorder"
 $exclusionList = @('eventlogs', 'UCSUploads')
 # Get all files in the current directory and its subdirectories
 $items = Get-ChildItem -Path $path -Recurse | Where-Object { !$_.PSIsContainer }
-# Filter out the items from the exclusion list and those that are not older than 7 days
-$itemsToDelete = $items | Where-Object { $_.LastWriteTime -lt (Get-Date).AddDays(-7) -and $_.Directory.Name -notin $exclusionList }
+# Filter out the items from the exclusion list and those that are not older than 1 day
+$itemsToDelete = $items | Where-Object { $_.LastWriteTime -lt (Get-Date).AddDays(-2) -and $_.Directory.Name -notin $exclusionList }
 # Delete the filtered items
 foreach ($item in $itemsToDelete) {
     Remove-Item $item.FullName -Force
