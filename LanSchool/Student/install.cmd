422ed53668d1696b4bec99d898cc4abfbfdc5283
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-04-11 12:32:54 -0400

    Added LanSchool files.

---
+++ LanSchool/Student/install.cmd
@@ -0,0 +1,8 @@
+REM Lanschool Student for ESJ2309
+msiexec.exe /i "\student.msi" /qn ADVANCED_OPTIONS=1 ENABLECHANNELSELECT=1 CHANNEL=11
+
+REM Lanschool Student for ESJB0320 
+msiexec.exe /i "\student.msi" /qn ADVANCED_OPTIONS=1 ENABLECHANNELSELECT=1 CHANNEL=12
+
+REM Lanschool Student for PLS 1129
+msiexec.exe /i "\student.msi" /qn ADVANCED_OPTIONS=1 ENABLECHANNELSELECT=1 CHANNEL=13
---
+++ LanSchool/Teacher/install.cmd
@@ -0,0 +1,9 @@
+
+REM Lanschool Student for ESJ2309
+msiexec.exe /i "\teacher.msi" /qn ADVANCED_OPTIONS=1 CHANNEL=11 LICENSING_KEY=
+
+REM Lanschool Student for ESJB0320
+msiexec.exe /i "\teacher.msi" /qn ADVANCED_OPTIONS=1 CHANNEL=12 LICENSING_KEY=
+
+REM Lanschool Student for PLS1129
+msiexec.exe /i "\teacher.msi" /qn ADVANCED_OPTIONS=1 CHANNEL=13 LICENSING_KEY=
