8198b61c873925e0b3e62b92ef5164d80f2ef5cf
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-04-16 11:43:44 -0400

    Added Message.

--- General/EmailMessage.ps1
+++ General/EmailMessage.ps1
@@ -1,27 +1,58 @@
+
+#variables
 $threshold = 64GB
-$freeSpace = Get-PSDrive -Name "C"  | Select-Object Free
+$disk = Get-PSDrive -Name "C"  
 
-if ($freeSpace.Free -lt $threshold){
 
+#Email Variables
 $EmailTo = "cmcleod1@umd.edu"
 $EmailFrom = "cmcleod1@umd.edu"
-$Subject = "Low Disk Space Warning"
-$Body = "Your device is running low on disk space."
 $SMTPServer = "smtp.gmail.com"
 $SMTPPort = 587
 $Username = "cmcleod1@umd.edu"
 $Password = "mzil wfmw gqhv wyae"  # This is the 16-character app password generated 
 
-$SMTPInfo = @{
-    To = $EmailTo
-    From = $EmailFrom
+#Funaction to send an email using Gmail SMTP server
+
+Function Send-Email {
+param (
+    [Parameter(Mandatory=$true)][string]$To,
+    [Parameter(Mandatory=$true)][string]$From,
+    [Parameter(Mandatory=$true)][string]$Body,
+    [Parameter(Mandatory=$true)][string]$Subject,
+    [Parameter(Mandatory=$true)][string]$SMTPServer,
+    [Parameter(Mandatory=$true)][string]$SMTPPort,
+    [Parameter(Mandatory=$true)][string]$Username,
+    [Parameter(Mandatory=$true)][string]$Password
+
+)
+
+$SecurePassword = $Password | ConvertTo-SecureString -AsPlainText -Force
+$SMTPCredential = New-Object System.Management.Automation.PSCredential ($Username, $SecurePassword)
+
+
+$MailMessage = @{
+    To = $To
+    From = $From
     Subject = $Subject
     Body = $Body
     SmtpServer = $SMTPServer
     Port = $SMTPPort
     UseSsl = $true
-    Credential = New-Object System.Management.Automation.PSCredential ($Username, ($Password | ConvertTo-SecureString -AsPlainText -Force))
+    Credential = $SMTPCredential
 }
 
-Send-MailMessage @SMTPInfo
+Send-MailMessage @MailMessage
+}
+# Check if free space is below the threshold
+if ($disk.Free -lt $threshold) {
+    $bodyText = "Warning: The drive $($disk.Name) on $($env:COMPUTERNAME) is running low on disk space. `nFree space left: $(($disk.Free / 1GB).ToString('N2')) GB out of $($thresholdGB) GB threshold."
+    $subjectText = "Low Disk Space Alert on $($env:COMPUTERNAME)"
+
+    # Sending the email
+    Send-Email -To $emailTo -From $emailFrom -Body $bodyText -Subject $subjectText -SMTPServer $smtpServer -SMTPPort $smtpPort -Username $username -Password $appPassword
+    Write-Host "Email sent successfully!"
+} else {
+    Write-Host "Disk space is above the threshold. No email sent."
+    Write-Host "Free space: $(($disk.Free / 1GB).ToString('N2')) GB"
 }
