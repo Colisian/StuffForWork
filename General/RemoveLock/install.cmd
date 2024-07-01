98903a1bd7ff615fa1f1671457353685ed316768
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-07-01 08:46:40 -0400

    Edited batch script.

--- General/RemoveLock/install.cmd
+++ General/RemoveLock/install.cmd
@@ -1,11 +1,18 @@
 @echo off
+
+Rem Ensure Perf Logs folder exists
+
+if not exist "C:\PerfLogs" (
+   mkdir "C:\PerfLogs"
+)
+
 REM Run the PowerShell script
-Powershell.exe -ExecutionPolicy Bypass -File "%TargetDir%\RemoveLock.ps1"
+Powershell.exe -ExecutionPolicy Bypass -File "RemoveLock.ps1"
 
 if %ERRORLEVEL% EQU 0 (
     echo Script ran successfully
     REM Create or update the file used for detection rules directly in the C: drive
-    echo %DATE% %TIME% > "C:\LastRun.txt"
+    echo %DATE% %TIME% > "C:\PerfLogs\LastRun.txt"
 ) else (
     echo Script failed to run
 )
