4bd387e3eb7f5ae0d583b15812555638ef1ec9f5
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-07-01 15:05:02 -0400

    Edited uninstall Remove Lock ps1 script

--- General/RemoveLock/UninstallRemoveLock.ps1
+++ General/RemoveLock/UninstallRemoveLock.ps1
@@ -1,17 +1,33 @@
 #define registry path
 $regpathSystem = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
 $regpathUser = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+$defaultUserRegPath = "HKU\$($_.PSChildName)\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+
+# Function to remove registry value
+function Remove-ItemProperty{
+    param (
+        [string]$path,
+        [string]$name
+    )
 
 if (Test-Path $regpath) {
     # Check if registry key exists 
-    $property = Get-ItemProperty -Path $regpath -Name "DisableLockWorkstation" -ErrorAction SilentlyContinue
+    $property = Get-ItemProperty -Path $path -Name $name -ErrorAction SilentlyContinue
     if ($property) {
         # Remove the DisableLockWorkstation
         Remove-ItemProperty -Path $regpath -Name "DisableLockWorkstation" -Force
         Write-Output "RemoveLock has been uninstalled successfully."
     } else {
         Write-Output "DisableLockWorkstation does not exist. RemoveLock is not installed"
+        } 
     } 
-} else {
-    Write-Output "Registry path does not exist."
 }
+
+# Remove the disable lock screen value to enable Lock workstation at System Level
+Remove-RegistryValue -path $regpathSystem -name "DisableLockWorkstation"
+
+# Remove the disable lock screen value to enable Lock workstation at User Level
+Remove-RegistryValue -path $regpathUser -name "DisableLockWorkstation"
+
+# Remove the disable lock screen value to enable Lock workstation at Default User Level
+Remove-RegistryValue -path $defaultUserRegPath -name "DisableLockWorkstation"
