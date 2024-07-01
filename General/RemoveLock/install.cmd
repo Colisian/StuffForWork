eb87d9114e4150fbbb02fa1e77aed4f377e2aa76
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-07-01 08:33:04 -0400

    Edit batch script.

--- General/RemoveLock/install.cmd
+++ General/RemoveLock/install.cmd
@@ -1,11 +1,14 @@
 @echo off
-set TargetDir=C:\IntuneApp
-
-if not exist "%TargetDir%" (
-   mkdir "%TargetDir%"
-)
-
-copy "%~dp0DisableLockOption.ps1" "%TARGET_DIR%\"
-
+REM Run the PowerShell script
 Powershell.exe -ExecutionPolicy Bypass -File "%TargetDir%\RemoveLock.ps1"
 
+if %ERRORLEVEL% EQU 0 (
+    echo Script ran successfully
+    REM Create or update the file used for detection rules directly in the C: drive
+    echo %DATE% %TIME% > "C:\LastRun.txt"
+) else (
+    echo Script failed to run
+)
+
+REM Exit
+exit /b 0
