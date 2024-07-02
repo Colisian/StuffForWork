f3ebf4ee79f0fabc0dbf0cb3b336a63aa2f6a774
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-07-02 09:42:33 -0400

    Edited Scheduled Task.

--- General/RemoveLock/RemoveLockTask.ps1
+++ General/RemoveLock/RemoveLockTask.ps1
@@ -1,6 +1,6 @@
 $taskname = "Disable Lock Workstation"
 $action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-ExecutionPolicy" Bypass -File C:\PerfLogs\RemoveLock.ps1
-$trigger = New-ScheduleTaskTrigger -AtLogOn
-$principle = New-ScheduledTaskPrincipal -UserId "NT Authority\Interactive" -LogonType Interactive -RunLevel Highest
+$trigger = New-ScheduledTaskTrigger -AtLogOn
+$principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\INTERACTIVE" -LogonType Interactive -RunLevel Highest
 
-Register-ScheduledTask -TaskName $taskname -Action $action -Trigger $trigger -Principal $principle
+Register-ScheduledTask -TaskName $taskname -Action $action -Trigger $trigger -Principal $principal
