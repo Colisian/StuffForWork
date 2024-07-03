ec40ea89df880059b2893e7fea0c7c971446329f
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-07-03 09:10:14 -0400

    Simplified task script.

--- General/RemoveLock/RemoveLockTask.ps1
+++ General/RemoveLock/RemoveLockTask.ps1
@@ -1,24 +1,13 @@
-
-$targetDirectory = "C:\Scripts"
-
-# Ensure the target directory exists
-if (-not (Test-Path $targetDirectory)) {
-    New-Item -Path $targetDirectory -ItemType Directory
-}
-
-$sourceScript = ".\RemoveLock.ps1"
-$destinationPath = Join-Path -Path $targetDirectory -ChildPath "RemoveLock.ps1"
-
-Copy-Item - Path $sourceScript -Destination $destinationPath -Force
-
-
-$scriptPath = "C:\PerfLogs\RemoveLock"
-
 $taskname = "Disable Lock Workstation"
-$scriptPath = $destinationPath
+$scriptPath = "C:\Scripts\RemoveLock.ps1"
 $trigger = New-ScheduledTaskTrigger -AtLogOn
 $action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$scriptPath`""
-$Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
-$principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\INTERACTIVE" -LogonType Interactive -RunLevel Highest
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
+$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 
-Register-ScheduledTask -TaskName $taskname -Action $action -Trigger $trigger -Settings $Settings  -Principal $principal
+try {
+    Register-ScheduledTask -TaskName $taskname -Action $action -Trigger $trigger -Settings $settings -Principal $principal
+} catch {
+    Write-Output "Error registering scheduled task: $_"
+}
+
