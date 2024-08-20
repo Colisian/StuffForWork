61181e3136879a61d2f518e0f412d83b2e59f8eb
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-08-20 10:59:23 -0400

    Removed refresh cache

--- InstallFonts/InstallFonts.ps1
+++ InstallFonts/InstallFonts.ps1
@@ -10,22 +10,14 @@ if (-not (Test-Path $destinationFolder)){
 }
 
 # Copy the fonts to the destination directory overwriting existing fonts
-$fonts = Get-ChildItem -Path $sourceFolder -Filter *.ttf, *.otf, *.ttc, *.PFB, *.pfm
+$fontExtensions = @("*.ttf", "*.otf", "*.ttc", "*.PFB", "*.pfm")
+$fonts = Get-ChildItem -Path $sourceFolder -Include $fontExtensions -File
 foreach ($font in $fonts){
     $destinationFontPath = Join-Path -Path $destinationFolder -ChildPath $font.Name
     try {
         Copy-Item -Path $font.FullName -Destination $destinationFontPath -Force -ErrorAction Stop
-        Wite-Output "Installed/Overwitten font: $($font.Name)"
+        Write-Output "Installed/Overwitten font: $($font.Name)"
     } catch {
         Write-Output "Error installing font: $($font.Name) - Errot: $($_.Exception.Message)"
     }
-}
-
-# Refresh the font cache
-try{
-    Write-Output "Refreshing font cache..."
-    $null = Start-Proess -FilePath "C:\Wndows\System32\RunDll32.exe" -ArgumentList "shell32.dll,Control_RunDLL desk.cpl,,0" -NoNewWindow -Wait -ErrorAction Stop
-    Write-Output "Font cache refreshed successfully"
-} catch {
-    Write-Output "Error refreshing font cache: $($_.Exception.Message)"
 }
