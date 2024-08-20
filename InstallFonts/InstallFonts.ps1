dc6daa586b98abaefdfddce1a38bc0cfa7eb9841
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-08-20 12:06:27 -0400

    Edited to adjust how fonts are copied from the source folder.

--- InstallFonts/InstallFonts.ps1
+++ InstallFonts/InstallFonts.ps1
@@ -10,8 +10,19 @@ if (-not (Test-Path $destinationFolder)){
 }
 
 # Copy the fonts to the destination directory overwriting existing fonts
-$fontExtensions = @("*.ttf", "*.otf", "*.ttc", "*.PFB", "*.pfm")
-$fonts = Get-ChildItem -Path $sourceFolder -Include $fontExtensions -File
+$fontExtensions = "*.ttf", "*.otf", "*.ttc", "*.PFB", "*.pfm"
+$fonts = @()
+foreach ($extension in $fontExtensions){
+    $fonts += Get-ChildItem -Path $sourceFolder -Filter $extension -File
+}
+# Check if there are fonts to install
+if ($fonts.Count -eq 0) {
+    Write-Output "No fonts found in the source folder."
+    exit 1
+}
+
+# Copy the fonts to the destination directory and register them
+
 foreach ($font in $fonts){
     $destinationFontPath = Join-Path -Path $destinationFolder -ChildPath $font.Name
     try {
