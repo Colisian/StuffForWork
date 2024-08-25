b74bf9b5a268ecb82a0ad7d90b789ee36743ed93
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-08-25 13:14:18 -0400

    Changes

--- General/SetDomain/install.cmd
+++ General/SetDomain/install.cmd
@@ -2,4 +2,6 @@
 @echo off
 REM This command runs the PowerShell script with administrative privileges.
 Powershell.exe -ExecutionPolicy Bypass -File "SetDomain.ps1" 
+Powershell.exe -ExecutionPolicy Bypass -File "BlockGP.ps1" 
+
 exit /B 0
