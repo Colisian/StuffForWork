1a878a58ad4a730c45632368e498461d69450af8
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-08-25 11:02:55 -0400

    Edited uninstall

--- General/SetDomain/uninstall.cmd
+++ General/SetDomain/uninstall.cmd
@@ -2,3 +2,16 @@
 
 REM Delete the DefaultDomainName registry key
 REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v DefaultDomainName /f
+
+REM Re-enable GlobalProtect VPN as a Sign-In Option by deleting the 'Disabled' value
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\Credential Providers\{25CA8579-1BD8-469c-B9FC-6AC45A161C18}\" /v Disabled /f
+
+REM Log the uninstallation
+set logFolder=C:\Scripts
+set logFile=%logFolder%\ScriptExecutionLog.txt
+if not exist %logFolder% mkdir %logFolder%
+echo Uninstall script executed on: %date% %time% >> %logFile%
+echo GlobalProtect sign-in option re-enabled. >> %logFile%
+echo DefaultDomainName registry key removed. >> %logFile%
+
+exit /B 0
