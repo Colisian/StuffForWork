4b4226659621c3d3aa58da98b71be6e738e155e6
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-09-11 06:45:21 -0400

    Comment edits.

--- General/FindLoginTime.ps1
+++ General/FindLoginTime.ps1
@@ -1,7 +1,7 @@
 #Specify the start and end date/time for logon events
 
-$startDate = Get-Date "2024-09-06 11:00:00"  # 11 AM, 9/6/2024
-$endDate = Get-Date "2024-09-09 09:00:00"    # 9 AM, 9/9/2024
+$startDate = Get-Date "2024-09-06 11:00:00"  # 11 AM, 9/6/2024 adjust as needed
+$endDate = Get-Date "2024-09-09 09:00:00"    # 9 AM, 9/9/2024 adjust as needed
 
 #Path to output text file
 $logFile = "C:\Scripts\LoginTimes.txt"
@@ -9,7 +9,7 @@ $logFile = "C:\Scripts\LoginTimes.txt"
 
 #Get all logon events from the Security log
 $logonEvents = Get-WinEvent -LogName Security  | Where-Object{
-    #filter for logoff events
+    #filter for logoff events which is 4634. Logon events are 4624
     $_.Id -eq 4634 -and $_.TimeCreated -ne $null -and
     $_.TimeCreated.ToLocalTime() -ge $startDate -and
     $_.TimeCreated.ToLocalTime() -le $endDate
