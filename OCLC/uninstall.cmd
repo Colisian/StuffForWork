2ce23ab85e0326f7ca8718b1574d913c2747fb39
Colisian <cmcleod1@umd.edu>
2025-01-15 10:38:30 -0500

    Edited git ignore and uninstal.cmd in OCLC.

--- OCLC/uninstall.cmd
+++ OCLC/uninstall.cmd
@@ -1,16 +1,18 @@
-rem Microsoft Access Database Engine 2010 {90140000-00D1-0409-1000-0000000FF1CE}
-rem OCLC Connexion client {106AE75F-9EFC-4721-BB06-DB6683EB8DA9}
+:: Microsoft Access Database Engine 2010 {90140000-00D1-0409-1000-0000000FF1CE}
+:: OCLC Connexion client {106AE75F-9EFC-4721-BB06-DB6683EB8DA9}
 
 @echo off
 :: Uninstall OCLC Connexion client
-msiexec /x {106AE75F-9EFC-4721-BB06-DB6683EB8DA9} /qn /norestart
+msiexec.exe /x {106AE75F-9EFC-4721-BB06-DB6683EB8DA9} /qn /norestart
 if %errorlevel% neq 0 (
+   echo Failed to uninstall Connexion Client. Exiting...
    exit /b 1 
 )
 
 :: Uninstall Microsoft Access Database Engine 2010
-msiexec /x {90140000-00D1-0409-1000-0000000FF1CE} /qn /norestart
+msiexec.exe /x {90140000-00D1-0409-1000-0000000FF1CE} /qn /norestart
 if %errorlevel% neq 0 (
+    echo Failed to uninstall Access Database Engine. Exiting...
    exit /b 1
 )
 
