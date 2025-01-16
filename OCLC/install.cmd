6a745df87ee1afe3d80dbfb4c09a9e18181d7583
Colisian <cmcleod1@umd.edu>
2025-01-16 08:12:41 -0500

    Edited install CMD and add a force to put app in Public Desktop.

--- OCLC/install.cmd
+++ OCLC/install.cmd
@@ -1,11 +1,5 @@
 @echo off
 
-:: Ensure the script runs as Administrator
-if not "%1"=="elevated" (
-    powershell -Command "Start-Process cmd -ArgumentList '/c %~0 elevated' -Verb RunAs"
-    exit /b
-)
-
 :: Run the PowerShell script
 powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0oclcdeploy.ps1"
 
--- OCLC/oclcdeploy.ps1
+++ OCLC/oclcdeploy.ps1
@@ -4,25 +4,18 @@ $ComServiceMsi = Join-Path -Path $PSScriptRoot -ChildPath "OCLC.Connexion.ComSer
 $AccessDatabaseEngine = Join-Path -Path $PSScriptRoot -ChildPath "accessdatabaseengine_X64.exe"
 
 # Check and install Connexion.msi
-if (Test-Path $ConnexionMsi) {
+
     Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$ConnexionMsi`" ALLUSERS=1 /qn /norestart" -Wait
-} else {
-    Write-Host "Connexion.msi not found at $ConnexionMsi"
-}
+
 
 # Check and install OCLC.Connexion.ComServiceDeploy.msi
-if (Test-Path $ComServiceMsi) {
     Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$ComServiceMsi`" ALLUSERS=1 /qn /norestart" -Wait
-} else {
-    Write-Host "OCLC.Connexion.ComServiceDeploy.msi not found at $ComServiceMsi"
-}
+
 
 # Check and install AccessDatabaseEngine
-if (Test-Path $AccessDatabaseEngine) {
+
     Start-Process -FilePath $AccessDatabaseEngine -ArgumentList "/quiet /norestart" -Wait
-} else {
-    Write-Host "accessdatabaseengine_X64.exe not found at $AccessDatabaseEngine"
-}
+
 
 # Define paths
 $TargetExe = "C:\Program Files\OCLC\Connexion\Program\Connex.exe"
