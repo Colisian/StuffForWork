ac473254d97697801ee3b2245be2afff2b4a4674
Colisian <cmcleod1@umd.edu>
2025-01-24 15:41:57 -0500

    Adjust Rhino Scripts

--- Rhino/uninstall.cmd
+++ Rhino/uninstall.cmd
@@ -1,3 +1,3 @@
 @echo off
-start /wait "" "%~dp0BurnOut.exe" /uninstall {851D74A0-D029-4AC7-9CBE-E50455C24237}
+"%~dp0BurnOut.exe" /uninstall {851D74A0-D029-4AC7-9CBE-E50455C24237}
 exit /b %errorlevel%
