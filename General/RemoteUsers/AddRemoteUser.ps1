731b889b209399d9f60e7d0c77a98812706d55b0
Colisian <cmcleod1@umd.edu>
2025-03-17 12:00:41 -0400

    Edited Script

--- General/RemoteUsers/AddRemoteUser.ps1
+++ General/RemoteUsers/AddRemoteUser.ps1
@@ -1,60 +1,61 @@
-#Load Windows forms assembly
+# Load Windows Forms assembly
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
-#Create and configure form
+# Create and configure the form
 $form = New-Object System.Windows.Forms.Form
-$form.Text = "Add Remote User Name"
+$form.Text = "Enter Directory Shortname"
 $form.Size = New-Object System.Drawing.Size(400,150)
 $form.StartPosition = "CenterScreen"
 
-#Create and configure label
+# Create a label for instructions
 $label = New-Object System.Windows.Forms.Label
-$label.Text = "Enter your directory name:"
+$label.Text = "Enter your directory shortname:"
 $label.AutoSize = $true
 $label.Location = New-Object System.Drawing.Point(10,20)
 $form.Controls.Add($label)
 
-#Create and configure text box
+# Create a textbox for user input
 $textBox = New-Object System.Windows.Forms.TextBox
-$textBox.Location = New-Object System.Drawing.Point(10,40)
+$textBox.Location = New-Object System.Drawing.Point(10,50)
 $textBox.Width = 360
 $form.Controls.Add($textBox)
 
-#Create and configure OK button
+# Create an OK button to submit input
 $okButton = New-Object System.Windows.Forms.Button
 $okButton.Text = "OK"
 $okButton.Location = New-Object System.Drawing.Point(290,80)
 $okButton.Add_Click({
+    # Store the textbox input in the form's Tag property
     $form.Tag = $textBox.Text
     $form.Close()
 })
 $form.Controls.Add($okButton)
 
-#Show the form as a dialog
-$form.ShowDialog | Out-Null
+# Show the form as a modal dialog
+$form.ShowDialog() | Out-Null
 
-#Get the user name from the form
+# Retrieve the entered directory shortname
 $userShortName = $form.Tag
 
-#Check if input
-if ([string]:: ::IsNullOrEmpty($userShortName)) {
-    Write-Host "No user name entered. Exiting script."
+# Check if the user provided any input
+if ([string]::IsNullOrEmpty($userShortName)) {
+    Write-Host "No input provided. Exiting."
     exit 1
 }
 
-#AzureAd account
-$azureAdUser = "AzureAD\$userShortName"
-Write-Host "Adding user '$azureAdUser' to the Remote Desktop Users group."
+# Construct the AzureAD account name
+$azureUser = "AzureAD\$userShortName"
+Write-Host "Attempting to add $azureUser to the Remote Desktop Users group..."
 
-#Get remote Desktop users Group
+# Get the Remote Desktop Users group object using ADSI
 $remoteDesktopGroup = [ADSI]"WinNT://$env:COMPUTERNAME/Remote Desktop Users,group"
 
-#Add user to the group
-try{
-    $remoteDesktopGroup.Add("WinNT://$azureADUser")
-    Write-Host "User '$azureAdUser' added to the Remote Desktop Users group."
+# Attempt to add the specified AzureAD user to the group
+try {
+    $remoteDesktopGroup.Add("WinNT://$azureUser")
+    Write-Host "Successfully added $azureUser to the Remote Desktop Users group."
 } catch {
-    Write-Host "Failed to add user '$azureAdUser' to the Remote Desktop Users group."
+    Write-Host "Error adding $azureUser: $_" -ForegroundColor Red
     exit 1
 }
