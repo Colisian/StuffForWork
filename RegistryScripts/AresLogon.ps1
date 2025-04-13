f2f7b966598e605d5a181b33f4f50fbe5f5e3a5e
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2025-04-13 10:49:01 -0400

    Registry Scripts for Atlas Apps.

---
+++ RegistryScripts/AresLogon.ps1
@@ -0,0 +1,20 @@
+#Define regirstry path and values
+$RegistryPath = "HKLM:\SOFTWARE\WOW6432Node\AtlasSystems\Ares"
+$ValueNames = "LogonSettingsPath"
+$valueData = "C:\Program Files (x86)\Atlas Systems\Ares\Areslivelogon.dbc"
+
+#Check if registry path exists
+if (-not (Test-Path $RegistryPath)) {
+    Write-Output "Registry path does not exist. Creating it..."
+    #Create registry path if it does not exist
+    New-Item -Path $RegistryPath -Force | Out-Null
+}
+
+#Update or Create the registry Value
+try {
+    Set-ItemProperty -Path $RegistryPath -Name $ValueNames -Value $valueData -Type String -Force
+    Write-Output "Registry value updated successfully."
+}
+catch {
+    Write-Output "Failed to update registry value: $_"
+}
