5a4b84cf89050bbfb8ebce0b19bf0be2cdb01d03
Colisian <cmcleod1@umd.edu>
2025-05-12 15:10:51 -0400

    Slight Edits

--- Openrefine/uninstall.cmd
+++ Openrefine/uninstall.cmd
@@ -1,3 +1,3 @@
 @echo off
-start /wait "" "Program Files(x86)\OpenRefine\unins000.exe" /VERYSILENT /SUPPRESSMSGBOXES /ALLUSERS /NORESTART
+start /wait "" "C:\Program Files (x86)\OpenRefine\unins000.exe" /VERYSILENT /SUPPRESSMSGBOXES /ALLUSERS /NORESTART
 exit /b %errorlevel%
