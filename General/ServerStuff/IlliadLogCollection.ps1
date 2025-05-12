53e22e3943d9e482c0b15e167c84d1d75d85f03a
Colisian <cmcleod1@umd.edu>
2025-05-12 10:08:43 -0400

    Slight Edits

--- General/ServerStuff/IlliadLogCollection.ps1
+++ General/ServerStuff/IlliadLogCollection.ps1
@@ -4,7 +4,7 @@ $destDir = "C:\illiadlogs"
 
 #Ensure Directory Exist
 
-if(-not (Test-Path $destDire)){
+if(-not (Test-Path $destDir)){
     New-Item -Path $destDir -ItemType Directory -Force | Out-Null
 }
 
@@ -14,7 +14,7 @@ Get-ChildItem -Path $destDir -File | Where-Object {$_.LastWriteTime -lt (Get-Dat
 #Copy files to destination
 $todayPrefix = (Get-Date).ToString('M.d')
 Get-ChildItem -Path $sourceDir -File | ForEach-Object {
-    $newName = "$todayPrefix'_$($_.Name)"
+    $newName = "$($todayPrefix)_$($_.Name)"
     Copy-Item -Path $_.FullName -Destination (Join-Path -Path $destDir -ChildPath $newName) -Force
 
 }
