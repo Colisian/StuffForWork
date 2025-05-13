f7c86a7ffc190de84d948a420bff36114122128d
Colisian <cmcleod1@umd.edu>
2025-05-13 11:45:47 -0400

    ChemDraw script

---
+++ ChemDraw/Install-ChemDraw.ps1
@@ -0,0 +1,22 @@
+# 1.Install VC++
+Start-Process -FilePath "$PSScriptRoot\vcredist_x64.exe" `
+    -ArgumentList "/quiet /norestart" `
+    -Wait
+
+# 2. Install WebVView2 Runtime
+Start-Process -FilePath "$PSScriptRoot\MicrosoftEdgeWebView2RuntimeInstallerX64.exe" `
+    -ArgumentList "/silent /install" `
+    -Wait
+
+
+# 3. Install ChemDraw MSI
+Start-Process -FilePath "msiexec.exe" `
+  -ArgumentList '/i', "`"$PSScriptRoot\ChemDraw.msi`"" , `
+                'REBOOT=ReallySuppress','/qb' -Wait
+
+# 4. Activation Files
+$activationDir = Join-Path $PSScriptRoot "Activation"
+
+Start-Process -FilePath (Join-Path $activationDir 'Activate.exe') `
+    -ArgumentList '23.0', 'IsInstaller', '/silent' `
+    -Wait
