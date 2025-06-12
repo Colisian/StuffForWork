fa61e1e19b30efab889dc36665d8d9022a42592b
Colisian <cmcleod1@umd.edu>
2025-06-12 10:54:08 -0400

    Slight edits

---
+++ AtlasRegistryScripts/IlliadLogon.ps1
@@ -0,0 +1,62 @@
+
+# File copy section
+
+Write-Output "Starting Illiad Logon Script"
+
+$destinationPath = "C:\Program Files (x86)\Illiad"
+$sourceFolder = Join-Path -Path $PSScriptRoot -ChildPath "Files"
+$filename = "Illiadlivelogon.dbc"
+$sourceFile = Join-Path -Path $sourceFolder -ChildPath $filename
+$destinationFile = Join-Path -Path $destinationPath -ChildPath $filename
+
+if(-not (Test-path -Path $destinationPath)) {
+    Write-Output "Destination folder does not exist. Creating it..."
+    try{
+    New-Item -Path $destinationPath -ItemType Directory -Force | Out-Null
+    }
+    catch {
+        Write-Output "Failed to create destination folder: $_"
+    }
+}
+
+#Check if the source file exists
+if (-not( Test-path -Path $sourceFile)) {
+    Write-Output "Source file does not exist. Exiting..."
+    exit
+}
+
+try{
+    Copy-Item -Path $sourceFile -Destination $destinationFile -Force
+    Write-Output "File copied successfully to $destinationFile"
+} catch {
+    Write-Output "Failed to copy file: $_"
+}
+
+if (Test-Path -Path $destinationFile) {
+    Write-Output "File exists at $destinationFile"
+} else {
+    Write-Output "File does not exist at $destinationFile"
+}
+
+
+
+#Define regirstry path and values
+$RegistryPath = "HKLM:\SOFTWARE\WOW6432Node\AtlasSystems\Illiad"
+$ValueNames = "LogonSettingsPath"
+$valueData = "C:\Program Files (x86)\Illiad\Illiadlivelogon.dbc"
+
+#Check if registry path exists
+if (-not (Test-Path $RegistryPath)) {
+    Write-Output "Registry path does not exist. Creating it..."
+    #Create registry path if it does not exist
+    New-Item -Path $RegistryPath -Force | Out-Null
+}
+
+#Update or Create the registry Value
+try {
+    Set-ItemProperty -Path $RegistryPath -Name $ValueNames -Value $valueData -Type String -Force
+    Write-Output "Registry value updated successfully."
+}
+catch {
+    Write-Output "Failed to update registry value: $_"
+}
